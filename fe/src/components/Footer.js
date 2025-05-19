0757c7602071b694925260170ddcee1fe180c3fd
Seo-Faper <dswhdcks@gmail.com>
2025-05-19 19:30:09 +0900

    feat: 푸터를 수정했습니다.

--- fe/src/components/Footer.js
+++ fe/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p>© 2025 Your Company. All rights reserved.</p>
+      <p>© 2025 Your Company. All rights reserved. CUHA is 어쩌고 저쩌고..</p>
     </footer>
   );
 }
