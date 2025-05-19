ddeec90ec12425f53fc6e15de0a0d0239e23e0bd
Seo-Faper <dswhdcks@gmail.com>
2025-05-19 19:25:11 +0900

    feat: 해더를 수정했습니다.

--- fe/src/components/Header.js
+++ fe/src/components/Header.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/join">Join</Link>
-      </nav>
-    </header>
+    <>
+      <style>{`
+        header {
+          background-color: #222;
+          padding: 1rem;
+          color: white;
+        }
+
+        nav {
+          display: flex;
+          justify-content: center;
+          gap: 2rem;
+        }
+
+        a {
+          color: white;
+          text-decoration: none;
+          font-weight: bold;
+        }
+
+        a:hover {
+          color: #61dafb;
+        }
+      `}</style>
+
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/join">Join</Link>
+        </nav>
+      </header>
+    </>
   );
 }
+
 export default Header;
---
+++ readme.md
@@ -0,0 +1,8 @@
+# how to run 
+# 키는법
+
+```
+1. cd fe
+2. npm i
+3. npm start
+```
